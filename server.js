8e53d027f998a247be81ca59096bef0166c4f791
Patrick <patrick@Patricks-iMac.local>
2023-01-28 17:11:40 -0800

    changing file structure

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const router = require("./assets/js/router")
+const router = require("./public/assets/js/router")
 
 
 const PORT = 3001;
@@ -13,10 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('../public'));
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, './index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 app.get("/notes", (req, res)=>
-  res.sendFile(path.join(__dirname,"./notes.html"))
+  res.sendFile(path.join(__dirname,"./public/notes.html"))
 )
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} `)
